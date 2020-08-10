9dc0a6e0eb172a633e0aeb1f51bd2166d5ba952f
Arcangel Catanio <arcangel_021@outlook.com>
2020-08-10 12:06:01 -0500

    Cargando el proyecto completo

--- P_Strategy/P_Strategy/Program.cs
+++ P_Strategy/P_Strategy/Program.cs
@@ -10,6 +10,57 @@ namespace P_Strategy
     {
         static void Main(string[] args)
         {
+            string dato = "";
+            double x = 0;
+            double y = 0;
+            double resultado = 0;
+            string opcion = "";
+
+            // Nuestra variable de referencia al algoritmo
+            // Instanciamos con un default para permitir compilacion correcta
+            IOperacion operacion = new Suma();
+
+            do
+            {
+                Console.WriteLine("1- Suma, 2- Resta, 3- Multiplicacion, 4- Division, 5- Salir");
+                opcion = Console.ReadLine();
+
+                if (opcion == "5")
+                    break;
+
+                Console.WriteLine("Dame el valor de a");
+                dato = Console.ReadLine();
+                x = Convert.ToDouble(dato);
+
+                Console.WriteLine("Dame el valor de b");
+                dato = Console.ReadLine();
+                y = Convert.ToDouble(dato);
+
+                // Selecionamos el algoritmo
+                switch (opcion)
+                {
+                    case "1":
+                        operacion = new Suma();
+                        break;
+                    case "2":
+                        operacion = new Resta();
+                        break;
+                    case "3":
+                        operacion = new Multiplicacion();
+                        break;
+                    case "4":
+                        operacion = new Division();
+                        break; 
+                    default:
+                        Console.WriteLine("No elegiste la opcion correcta");
+                        break;
+                }
+
+                resultado = operacion.operacion(x, y);
+                Console.WriteLine("El resultado es {0}", resultado, ConsoleColor.Blue);
+                Console.WriteLine();
+
+            } while (opcion != "5");
         }
     }
 }
